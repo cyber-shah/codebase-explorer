134e7609438a1537fd624c0079b8eb267aabbf28
cyber-shah <pranchx@gmail.com>
2023-12-15 20:02:49 -0500

    scanning all foldres now

--- src/fileManager.cpp
+++ src/fileManager.cpp
@@ -2,19 +2,33 @@
 #include <fstream>
 #include <iostream>
 #include <sstream>
+#include <string>
 
 #include "fileManager.h"
 
 using namespace std;
-
+namespace fs = std::filesystem;
+// Alias for the experimental filesystem
 
 // Constructor implementation
 FileManager::FileManager() {
-    // Constructor logic (if needed)
+  // Constructor logic (if needed)
 }
 
 // Implementation of the run method
 void FileManager::run() {
-    // Your run method implementation
-    std::cout << "filemanager started" << std::endl;
+  string current_dir = fs::current_path().string();
+  cout << "Current Directory: " << current_dir << std::endl;
+
+  int file_number = 0;
+  // Scan files in the current directory
+  for (const auto &entry : fs::directory_iterator(current_dir)) {
+
+    if (entry.is_regular_file()) {
+      cout << "File " << file_number << ": " << entry.path() << std::endl;
+      file_number++;
+      // Process each file
+      // analyzeFile(entry.path().string());
+    }
+  }
 }
