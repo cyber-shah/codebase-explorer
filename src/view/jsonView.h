1bb4eb66bf277b33a09ecb970eda5d0770a61c39
cyber-shah <pranchx@gmail.com>
2023-12-26 22:31:32 -0500

    jsontrees

--- src/view/jsonView.h
+++ src/view/jsonView.h
@@ -1,14 +1,22 @@
 #ifndef JSONVIEW_H
 #define JSONVIEW_H
-
 #include "viewInterface.h"
+#include <nlohmann/json.hpp>
+
+using json = nlohmann::json;
 
 class jsonView : public viewInterface {
 public:
+  json dir_tree;
+  json dep_tree;
+  json cpu_tree;
+  json mem_tree;
+
   // Constructor
   jsonView() {}
 
-  void show_dir_tree()
-};
+  void show_dir_tree(const std::shared_ptr<nodeInterface> root) const override{
+
+  };
 
 #endif // DEBUG
