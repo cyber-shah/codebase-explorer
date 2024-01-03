735f28098c4c6ce36aabf4adbfe5010114de8447
cyber-shah <pranchx@gmail.com>
2024-01-02 20:22:21 -0500

    updating

--- frontend/src/Components/LinePlot.js
+++ frontend/src/Components/LinePlot.js
@@ -1,30 +1,53 @@
 import * as d3 from "d3";
 import React, { useRef, useEffect } from "react";
 
-export default function LinePlot({
-  data,
-}) {
+
+const drag = simulation => {
+
+  function dragstarted(event, d) {
+    if (!event.active) simulation.alphaTarget(0.3).restart();
+    d.fx = d.x;
+    d.fy = d.y;
+  }
+
+  function dragged(event, d) {
+    d.fx = event.x;
+    d.fy = event.y;
+  }
+
+  function dragended(event, d) {
+    if (!event.active) simulation.alphaTarget(0);
+    d.fx = null;
+    d.fy = null;
+  }
+
+  return d3.drag()
+    .on("start", dragstarted)
+    .on("drag", dragged)
+    .on("end", dragended);
+}
+
+
+
+
+
+export default function LinePlot(props) {
 
   const ref = useRef();
 
   useEffect(() => {
+
+    // ------------------------ SVG SETUP ------------------------
     // width of the svg
     const width = 928;
-
     // tells d3 to use the data as a hierarchy and to use the dir_children property for the children
-    const root = d3.hierarchy(data, d => d.dir_children);
-
+    const root = d3.hierarchy(props.data, d => d.dir_children);
     // set the width between nodes
-    const dx = 150;
-    const dy = 20;
-
-    // Create a tree layout.
+    const dx = props.dx ? props.dx : 150;
+    const dy = props.dy ? props.dy : 20;
+      // Create a tree layout.
     const tree = d3.tree().nodeSize([dy, dx]);
-
-    // Sort the tree and apply the layout.
-    root.sort((a, b) => d3.ascending(a.data.name, b.data.name));
     tree(root);
-
     // Compute the extent of the tree. Note that x and y are swapped here
     // because in the tree layout, x is the breadth, but when displayed, the
     // tree extends right rather than down.
@@ -34,21 +57,24 @@ export default function LinePlot({
       if (d.x > x1) x1 = d.x;
       if (d.x < x0) x0 = d.x;
     });
-
     // Compute the adjusted height of the tree.
     const height = x1 - x0 + dx * 2;
 
+    // create the svg
     const svg = d3.create("svg")
       .attr("width", width)
       .attr("height", height)
       .attr("viewBox", [-dy / 3, x0 - dx, width, height])
-      .attr("style", "max-width: 100%; height: auto; font: 10px sans-serif;");
+      .attr("style", "max-width: 100%; height: auto; font: 12px sans-serif;");
+    // ------------------------ SVG SETUP ------------------------
 
+    // link is the connection between nodes
+    // TODO: learn how this works
     const link = svg.append("g")
       .attr("fill", "none")
-      .attr("stroke", "#555")
-      .attr("stroke-opacity", 0.4)
-      .attr("stroke-width", 1.5)
+      .attr("stroke", props.stroke ? props.stroke : "#555")
+      .attr("stroke-opacity", props.strokeOpacity ? props.strokeOpacity : 0.4)
+      .attr("stroke-width", props.strokeWidth ? props.strokeWidth : 1.5)
       .selectAll()
       .data(root.links())
       .join("path")
@@ -56,6 +82,7 @@ export default function LinePlot({
         .x(d => d.y)
         .y(d => d.x));
 
+    // ------------------ NODE --------------------------
     const node = svg.append("g")
       .attr("stroke-linejoin", "round")
       .attr("stroke-width", 3)
@@ -75,13 +102,16 @@ export default function LinePlot({
       .text(d => d.data.name)
       .clone(true).lower()
       .attr("stroke", "white");
+    // ------------------ NODE --------------------------
 
     d3.select(ref.current).node().appendChild(svg.node());
-    // Return a cleanup function that removes the SVG.
-    return () => {
-      d3.select(ref.current).selectAll("svg").remove();
+    //  Return a cleanup function that removes the SVG.
+  return () => {
+  d3.select(ref.current).selectAll("svg").remove();
     };
-  }, []);
+        }, 
+
+  ); 
 
 
   return <div ref={ref}></div>;
