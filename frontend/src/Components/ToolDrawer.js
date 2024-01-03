3a3fcdf874bd76257183de4b378769e5bd00162b
cyber-shah <pranchx@gmail.com>
2024-01-02 22:05:38 -0500

    sliders

--- frontend/src/Components/ToolDrawer.js
+++ frontend/src/Components/ToolDrawer.js
@@ -1,17 +1,18 @@
-import { Box, Paper } from '@mui/material';
+import { Box, Divider, FormControl, InputLabel, MenuItem, Paper, Select, Stack } from '@mui/material';
 import React from 'react';
-import Stack from '@mui/material/Stack';
-import Slider from '@mui/material/Slider';
-import VolumeDown from '@mui/icons-material/VolumeDown';
-import VolumeUp from '@mui/icons-material/VolumeUp';
 import { Container, Typography, Button, TextField } from '@mui/material';
 import { useState } from 'react';
+import InputSlider from './ToolDrawer/Slider';
+
 
 
 export default function ToolDrawer(props) {
 
   const [currentDirectory, setCurrentDirectory] = useState('/');
-
+  const [age, setAge] = React.useState('');
+  const handleChange = (event) => {
+    setAge(event.target.value);
+  };
 
   return (
     <Paper elevation={3}>
@@ -26,7 +27,45 @@ export default function ToolDrawer(props) {
           Select Files
         </Button>
       </Container>
-    </Paper>
+
+      <Divider />
+
+      <Box sx={{
+        width: '90%',
+        margin: 'auto',
+        display: 'flex',
+        alignItems: 'center'
+      }}>
+        <Stack>
+          <FormControl fullWidth>
+            <InputLabel id="demo-simple-select-label">Tree Type</InputLabel>
+            <Select
+              labelId="demo-simple-select-label"
+              id="demo-simple-select"
+              value={age}
+              label="Directory Tree"
+              onChange={handleChange}
+            >
+              <MenuItem value={0}>Directory Tree</MenuItem>
+              <MenuItem value={1}>Dependency Tree</MenuItem>
+              <MenuItem value={2}> Resource Tree </MenuItem>
+            </Select>
+          </FormControl>
+
+          <InputSlider name='Node Size' />
+
+
+          <InputSlider name='Gap - X' />
+
+          < InputSlider name='Gap - Y' />
+
+          < InputSlider name='Link Color' />
+        </Stack>
+
+      </Box>
+
+      < Divider />
+    </Paper >
 
   );
 }
