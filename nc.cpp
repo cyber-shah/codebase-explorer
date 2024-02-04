dce7f2d12602b7adc6e761cc9ee135790d0f3270
cyber-shah <shah.pranc@northeastern.edu>
2024-02-04 14:05:17 -0500

    profoling works

--- nc.cpp
+++ nc.cpp
@@ -1,35 +1,14 @@
 
-#include <chrono>
-#include <gperftools/profiler.h>
+#include <cmath>
 #include <iostream>
-#include <thread>
-
-void performIntensiveTask() {
-  // Simulate some CPU-intensive work
-  for (int i = 0; i < 1000000; ++i) {
-    double result = sqrt(i * 3.14);
-    // Avoid compiler optimization by using the result
-    if (result < 0)
-      std::cout << "Should not happen!";
-  }
-}
 
 int main() {
-  // Start CPU profiling
-  ProfilerStart("profile_output.prof");
-
-  // Your program logic goes here
-
-  std::cout << "Performing intensive task..." << std::endl;
-  performIntensiveTask();
-
-  // Simulate some additional work
-  std::this_thread::sleep_for(std::chrono::seconds(2));
-
-  std::cout << "Done with intensive task." << std::endl;
-
-  // Stop CPU profiling
-  ProfilerStop();
-
+  for (int i = 0; i < 1000000; ++i) {
+    double result = 0.0;
+    for (int j = 0; j < 100; ++j) {
+      result += std::sin(j) * std::cos(j);
+    }
+    std::cout << result << std::endl;
+  }
   return 0;
 }
